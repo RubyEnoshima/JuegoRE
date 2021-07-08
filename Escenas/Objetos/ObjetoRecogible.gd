ad45efd21ac04c07ab9d0122c55439a01592251f
RubyEnoshima <vilaruben70@gmail.com>
2021-07-08 23:22:50 +0200

    Menú en desarrollo

--- Escenas/Objetos/ObjetoRecogible.gd
+++ Escenas/Objetos/ObjetoRecogible.gd
@@ -2,22 +2,49 @@ class_name ObjetoRecogible
 extends Area2D
 
 var aura = preload("res://aura.tres")
+var interactuado = false
 var focus = false
+var jugadorCerca = false
 
 func _ready():
 	input_pickable = true
+	#Warning porque me conozco
+	if $Jugador/CollisionShape2D.shape == null:
+		print("El objeto ",name," no tiene un area de Jugador delimitada!!")
+	if $TextoInteractuado.text == "":
+		print("El objeto ",name," no tiene texto!")
 
 func _process(delta):
-	if focus and Input.is_action_just_pressed("coger"):
+	if focus and Input.is_action_just_pressed("coger") and jugadorCerca:
 		interactuar()
+	if $TextoInteractuado.visible:
+		$TextoInteractuado.rect_position.y -= 0.3
 
 func interactuar():
-	queue_free()
+	interactuado = true
+	$Sprite.queue_free()
+	$TextoInteractuado.visible = true
+	$Timer.start()
 
 func _on_Objeto_mouse_entered():
-	$Sprite.material = aura
-	focus = true
+	if !interactuado and jugadorCerca:
+		$Sprite.material = aura
+		focus = true
+		
 
 func _on_Objeto_mouse_exited():
-	$Sprite.material = null
-	focus = false
+	if !interactuado and jugadorCerca:
+		$Sprite.material = null
+		focus = false
+
+func _on_Timer_timeout():
+	queue_free()
+
+func _on_Jugador_body_entered(body):
+	if body is Personaje and !interactuado:
+		jugadorCerca = true
+
+func _on_Jugador_body_exited(body):
+	if body is Personaje and !interactuado:
+		$Sprite.material = null
+		jugadorCerca = false
--- Escenas/Personaje/Personaje.gd
+++ Escenas/Personaje/Personaje.gd
@@ -1,6 +1,9 @@
 class_name Personaje
 extends KinematicBody2D
 
+var Menu = preload("res://Escenas/Menu/Menu.tscn")
+var enMenu = false
+
 export (int) var speed = 115
 export (int) var gravity = 4000
 export var vida = 100
@@ -28,42 +31,53 @@ func pisadas():
 		$pisadas.play()
 
 func get_input():
-	velocity.x = 0
-	if Input.is_action_pressed("mov_der"):
-		pisadas()
-		velocity.x += speed
-		#$Arma.disabled = true
+	if !enMenu:
+		velocity.x = 0
+		if Input.is_action_pressed("mov_der"):
+			pisadas()
+			velocity.x += speed
+			#$Arma.disabled = true
+		
+		if Input.is_action_pressed("mov_izq"):
+			pisadas()
+			velocity.x -= speed
+			#$Arma.disabled = true
 	
-	if Input.is_action_pressed("mov_izq"):
-		pisadas()
-		velocity.x -= speed
-		#$Arma.disabled = true
+		if Inventario.pistola: #Esto esta mal!!!!!!!!!!!!
+			if Input.is_action_just_pressed("disparo"):
+				$Arma.disparar()
+			elif Input.is_action_just_pressed("recargar"):
+				$Arma.recargar()
+		
+		if !velocity.x or Input.is_action_just_released("mov_der") or Input.is_action_just_released("mov_izq"):
+			$pisadas.stop()
+		
+		if Input.is_action_pressed("correr") and !corriendo:
+			corriendo = true
+			speed *= 2.7
+		elif Input.is_action_just_released("correr") and corriendo:
+			corriendo = false
+			speed /= 2.7
+		
+		var mouse = get_global_mouse_position()
+		if mouse.x >= position.x:
+			if !derecha:
+				derecha = true
+			$Sprite.flip_h = !$Sprite.flip_h
+		elif mouse.x < position.x:
+			if derecha:
+				derecha = false
+			$Sprite.flip_h = !$Sprite.flip_h
+		
 	
-	if !velocity.x or Input.is_action_just_released("mov_der") or Input.is_action_just_released("mov_izq"):
-		$pisadas.stop()
-	
-	if Input.is_action_pressed("correr") and !corriendo:
-		corriendo = true
-		speed *= 2.7
-	elif Input.is_action_just_released("correr") and corriendo:
-		corriendo = false
-		speed /= 2.7
-	
-	var mouse = get_global_mouse_position()
-	if mouse.x >= position.x:
-		if !derecha:
-			derecha = true
-		$Sprite.flip_h = !$Sprite.flip_h
-	elif mouse.x < position.x:
-		if derecha:
-			derecha = false
-		$Sprite.flip_h = !$Sprite.flip_h
-	
-	if Inventario.pistola: #Esto esta mal!!!!!!!!!!!!
-		if Input.is_action_just_pressed("disparo"):
-			$Arma.disparar()
-		elif Input.is_action_just_pressed("recargar"):
-			$Arma.recargar()
+	if Input.is_action_just_pressed("menu"):
+		var m = Menu.instance()
+		var children = get_parent().get_children()
+		for child in children:
+			child.visible = false
+		get_parent().add_child(m)
+		enMenu = true
+		#get_tree().change_scene("res://Escenas/Menu/Menu.tscn")
 
 func _physics_process(delta):
 	get_input()
